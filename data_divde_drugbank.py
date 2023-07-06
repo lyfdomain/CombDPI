6801e517aa6c71e59571880d8d145c857c55195d
lyfdomain <103353319+lyfdomain@users.noreply.github.com>
2023-07-06 18:06:29 +0800

    Update data_divde_drugbank.py

--- data_divde_drugbank.py
+++ data_divde_drugbank.py
@@ -25,7 +25,7 @@ random.shuffle(list_1)
 #
 array_1=np.array(list_1)[0:fold*group_size1]
 group_data1=np.reshape(array_1,(fold,group_size1))
-np.savetxt('./dataset/drugbank/corssval/index_1.txt',group_data1)
+np.savetxt('./dataset/drugbank/crossval/index_1.txt',group_data1)
 
 
 i=0
@@ -41,7 +41,7 @@ random.shuffle(list_0)
 
 array_0=np.array(list_0)[0:fold*group_size0]
 group_data0=np.reshape(array_0,(fold,group_size0))
-np.savetxt('./dataset/drugbank/corssval/index_0.txt',group_data0)
+np.savetxt('./dataset/drugbank/crossval/index_0.txt',group_data0)
 
 
 f = 0
@@ -53,7 +53,7 @@ for f in trange(fold):
         c = int(group_data1[f, i] % prenum)
         DTI[r, c] = 0
         i += 1  # 得到每次交叉验证中所使用的A矩阵
-    np.savetxt('./dataset/drugbank/corssval/DTI'+str(f)+".txt",DTI)
+    np.savetxt('./dataset/drugbank/crossval/DTI'+str(f)+".txt",DTI)
 
 
 
